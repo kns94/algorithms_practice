1d09f445bb4bd3a8b001407fe319edf744a820e5
kushals@uw.edu <kns971@gmail.com>
2016-12-14 15:38:09 -0800

    Few more changes

--- KMP_prefix.py
+++ KMP_prefix.py
@@ -3,13 +3,13 @@ def buildPrefixArray(string):
     temp = [0] * len(string)
     temp[0] = 0
     i = 1
-    len = 0
+    l = 0
 
     while i < len(string):
-        if string[i] == string[len]:
-            temp[i] = len + 1
+        if string[i] == string[l]:
+            temp[i] = l + 1
             i += 1
-            len += 1
+            l += 1
         else:
             if j > 0:
                 j = temp[j - 1]
--- twosum_two.py
+++ twosum_two.py
@@ -6,3 +6,17 @@ class Solution(object):
         :rtype: List[int]
         """
         
+        l = 0
+        r = len(numbers) - 1
+        #print l, r
+
+        while l < len(numbers) and r > 0:
+            total = numbers[l] + numbers[r]
+            if total == target:
+                return[l + 1, r + 1]
+            elif total < target:
+                l += 1
+            else:
+                r -= 1
+
+print Solution().twoSum([2, 7, 11, 15], 9)
